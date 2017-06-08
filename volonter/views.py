9272e27d6be16e0695fd8b14354b4798680c995e
Tartar-san <secondmailgreen@gmail.com>
2017-06-08 12:04:05 +0300

    One base for templates

--- volonter/views.py
+++ volonter/views.py
@@ -10,71 +10,71 @@ def main_page(request): #
 
 def organization_inside(request):
     if (request.user.is_authenticated):
-        return render(request, 'organization-inside.html')
+        return render(request, 'organization-inside.html', context={'logged_in': True})
     else:
-        return render(request, 'organization-inside.html')
+        return render(request, 'organization-inside.html', context={'logged_in': False})
 
 
 def organization_outside(request):
     if (request.user.is_authenticated):
-        return render(request, 'organization-outside.html')
+        return render(request, 'organization-outside.html', context={'logged_in': True})
     else:
-        return render(request, 'organization-outside.html')
+        return render(request, 'organization-outside.html', context={'logged_in': False})
 
 
 def event_inside(request):
     if (request.user.is_authenticated):
-        return render(request, 'event-inside.html')
+        return render(request, 'event-inside.html', context={'logged_in': True})
     else:
-        return render(request, 'event-inside.html')
+        return render(request, 'event-inside.html', context={'logged_in': False})
 
 
 def event_outside(request):
     if (request.user.is_authenticated):
-        return render(request, 'event-outside.html')
+        return render(request, 'event-outside.html', context={'logged_in': True})
     else:
-        return render(request, 'event-outside.html')
+        return render(request, 'event-outside.html', context={'logged_in': False})
 
 
 def search(request):
     if (request.user.is_authenticated):
-        return render(request, 'search.html')
+        return render(request, 'search.html', context={'logged_in': True})
     else:
-        return render(request, 'search.html')
+        return render(request, 'search.html', context={'logged_in': False})
 
 
 def our_team(request):
     if (request.user.is_authenticated):
-        return render(request, 'our-team.html')
+        return render(request, 'our-team.html', context={'logged_in': True})
     else:
-        return render(request, 'our-team.html')
+        return render(request, 'our-team.html', context={'logged_in': False})
 
 
 def enter(request):
     if (request.user.is_authenticated):
-        return render(request, 'enter.html')
+        return render(request, 'enter.html', context={'logged_in': True})
     else:
-        return render(request, 'enter.html')
+        return render(request, 'enter.html', context={'logged_in': False})
 
 
 def user_inside(request):
     if (request.user.is_authenticated):
-        return render(request, 'user-inside.html')
+        return render(request, 'user-inside.html', context={'logged_in': True})
     else:
-        return render(request, 'user-inside.html')
+        return render(request, 'user-inside.html', context={'logged_in': False})
 
 
 def user_outside(request):
     if (request.user.is_authenticated):
-        return render(request, 'user-outside.html')
+        return render(request, 'user-outside.html', context={'logged_in': True})
     else:
-        return render(request, 'user-outside.html')
+        return render(request, 'user-outside.html', context={'logged_in': False})
 
 def about_us(request):
     if (request.user.is_authenticated):
-        return render(request, 'about-us.html')
+        return render(request, 'about-us.html', context={'logged_in': True})
     else:
-        return render(request, 'about-us.html')
+        return render(request, 'about-us.html', context={'logged_in': False})
 
 
 
