634f6e48b2a993b925c17edd981fe03c5523051a
yarycka <yaryna.korduba@gmail.com>
2017-06-27 14:28:06 +0300

    changed caousel images

--- Volonterity/settings.py
+++ Volonterity/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = '-nj-wd39mbli##3ix8vp7=lzeji(a*vq++d*7d9tfdbtw*yrlg'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['10.0.128.98']
+ALLOWED_HOSTS = ['10.0.128.98', '127.0.0.1']
 
 
 # Application definition
--- static/js/main-header.js
+++ static/js/main-header.js
@@ -1,20 +1,17 @@
 $(window).on("scroll", function() {
 
-    if($(window).scrollTop() > 50) {
+    if ($(window).scrollTop() <= 50) {
+        //remove the background property so it comes transparent again (defined in your css)
+        $(".navbar-fixed-top").removeClass("active");
+        $(".navbar-fixed-top").css("background", "rgb(0,0,0,0.65)");
+
+
+    } else {
         $(".navbar-fixed-top").addClass("active");
         $(".navbar-fixed-top").css("background", "rgb(0,0,0,0.50)"); //.....
+        $(".panel-default").addClass("animated zoomIn");
 
     }
 
-
-    else {
-        //remove the background property so it comes transparent again (defined in your css)
-       $(".navbar-fixed-top").removeClass("active");
-       $(".navbar-fixed-top").css("background", "rgb(0,0,0,0.65)");
-
-
-    }
-
-    $(".panel").addClass("animated zoomIn");
 
 });
